37b356ded6bea116e202c687a6b0cf338ef0d5a8
answer212224 <answer212224@gmail.com>
2021-03-23 15:22:01 +0800

    search

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Animal;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AnimalFactory extends Factory
@@ -22,7 +23,15 @@ class AnimalFactory extends Factory
     public function definition()
     {
         return [
-            //
+            // numberBetween隨機產生範圍1到3之間的整數。
+            'type_id' => $this->faker->numberBetween(1, 3),
+            'name' => $this->faker->name, // 隨機名稱
+            'birthday' => $this->faker->date(),  // 隨機日期
+            'area' => $this->faker->city, // 隨機城市名稱
+            'fix' => $this->faker->boolean, // 隨機布林值
+            'description' => $this->faker->text,  // 隨機一段內容
+            'personality' => $this->faker->text, // 隨機一段內容
+            'user_id' => User::all()->random()->id // 隨機綁定一位會員
         ];
     }
 }
